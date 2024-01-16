9d97c5079e2efaf338a0a62e23581242c6369f5b
alireza <121108490+alireza64ahmadi@users.noreply.github.com>
2024-01-16 04:59:44 +0330

    “Added Quiz Questions Data”
     “Integrated an array of quiz questions and their respective answer choices into the project, allowing for the dynamic population of quiz content. Use the copy button to copy this commit information.”

---
+++ app.js
@@ -0,0 +1,38 @@
+const questions = [
+    {
+        question : "Which is the tallest tower in the world?",
+        answer:[
+            { text : "Milad Tower" , correct : false  },
+            { text : "Pizza Tower" , correct : false  },
+            { text : "Eiffel Tower" , correct : false  },
+            { text : "Khalifa Tower" , correct : true  }
+        ]
+    },
+    {
+        question : "Which is the smallest continent in the world?",
+        answer:[
+            { text : "Asia" , correct : false  },
+            { text : "Australia" , correct : true  },
+            { text : "Africa" , correct : false  },
+            { text : "North America" , correct : false  }
+        ]
+    },
+    {
+        question : "Which is the laegest deseret in the world?",
+        answer:[
+            { text : "Kalahari" , correct : false  },
+            { text : "Gobi" , correct : false  },
+            { text : "Sahara" , correct : false  },
+            { text : "Antarctica" , correct : true  }
+        ]
+    },
+    {
+        question : "Which is the smallest country in the world?",
+        answer:[
+            { text : "Vatican City" , correct : true  },
+            { text : "Bhutan" , correct : false  },
+            { text : "Nepal" , correct : false  },
+            { text : "Sri Lanka" , correct : false  }
+        ]
+    },
+]
